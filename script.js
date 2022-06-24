6639fa2047c7db02c522438e78fedecec8d08dcb
beaxxsantos <beasantos.rj@outlook.com>
2022-06-24 09:12:10 -0300

    if/else

---
+++ script.js
@@ -0,0 +1,32 @@
+
+{let idade = 24
+    if (idade>18){
+    console.log("Isso ai, você já pode votar. Vote direito!")
+    }else
+    {
+    console.log("Ih, não pode ser preso e nem é obrigado a votar...") 
+    }
+    }
+   
+
+    {let idade = 16
+    let humano = true
+    
+    if (idade>18 && humano){
+    console.log("Humano e maior de idade isso quer dizer que você ta na terra e ta ferrado.")
+    }else
+    {
+    console.log("Já pensou em voltar pro planeta de origem?")
+    }
+    }
+    
+    /*Exercício 3 */
+    {let mes = "janeiro"
+    
+    if( mes == "janeiro" || mes == "dezembro"){
+    console.log("Você nasceu nas ferias!")
+    }else
+    {
+    console.log("Sinto muito só capri entram.")
+    }
+    }
